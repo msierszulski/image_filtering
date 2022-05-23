ae8e02800f750cbb6e66714e569a0f3c86fd2dae
Mateusz Sierszulski <mateusz.sierszulski@gmail.com>
2022-05-23 23:11:24 +0200

    Add median filtering

--- main.c
+++ main.c
@@ -8,23 +8,17 @@
 
 typedef struct
 {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
-} rgb;
-
-typedef struct
-{
     char p;
     int format;
     int width;
     int height;
     int intensity;
-    rgb **pixels;
+    unsigned char **pixels;
 } image;
 
-void read_ppm_file(FILE *fin, image *img);
-void write_ppm_file(FILE *fout, image *img);
+void read_pgm_file(FILE *fin, image *img);
+void write_pgm_file(FILE *fout, image *img);
+void median_filter(image *img, int window);
 
 int main(int argc, char *argv[])
 {
@@ -45,40 +39,42 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    read_ppm_file(fin, &img);
+    read_pgm_file(fin, &img);
 
-    write_ppm_file(fout, &img);
+    median_filter(&img, 3);
+
+    write_pgm_file(fout, &img);
 
     fclose(fin);
     fclose(fout);
     return 0;
 }
 
-void read_ppm_file(FILE *fin, image *img)
+void read_pgm_file(FILE *fin, image *img)
 {
     fscanf(fin, "%c%d\n", &img->p, &img->format);
     fscanf(fin, "%d %d\n", &img->width, &img->height);
     fscanf(fin, "%d\n", &img->intensity);
 
-    img->pixels = (rgb **)malloc(img->height * sizeof(rgb *));
+    img->pixels = (unsigned char **)malloc(img->height * sizeof(unsigned char *));
 
     for (int i = 0; i < img->height; i++)
     {
-        img->pixels[i] = (rgb *)malloc(img->width * sizeof(rgb));
+        img->pixels[i] = (unsigned char *)malloc(img->width * sizeof(unsigned char));
     }
 
     for (int i = 0; i < img->height; i++)
     {
         for (int j = 0; j < img->width; j++)
         {
-            fread(&img->pixels[i][j], sizeof(unsigned char), 3, fin);
+            fread(&img->pixels[i][j], sizeof(unsigned char), 1, fin);
         }
     }
 }
 
-void write_ppm_file(FILE *fout, image *img)
+void write_pgm_file(FILE *fout, image *img)
 {
-    fprintf(fout, "P6\n");
+    fprintf(fout, "P5\n");
     fprintf(fout, "%d %d\n", img->width, img->height);
     fprintf(fout, "%d\n", img->intensity);
 
@@ -86,7 +82,47 @@ void write_ppm_file(FILE *fout, image *img)
     {
         for (int j = 0; j < img->width; j++)
         {
-            fwrite(&img->pixels[i][j], sizeof(unsigned char), 3, fout);
+            fwrite(&img->pixels[i][j], sizeof(unsigned char), 1, fout);
+        }
+    }
+}
+
+unsigned char img_get_value(image *img, int width, int height)
+{
+    if (width >= 0 && width < img->width && height >= 0 && height < img->height)
+    {
+        return img->pixels[height][width];
+    }
+    else
+    {
+        return '\0';
+    }
+}
+
+int compare(const void *a, const void *b)
+{
+    return (*(unsigned char *)a - *(unsigned char *)b);
+}
+
+void median_filter(image *img, int window)
+{
+    unsigned char *matrix;
+
+    matrix = (unsigned char *)malloc(window * window * sizeof(unsigned char));
+
+    for (int y = 0; y < img->width; y++)
+    {
+        for (int x = 0; x < img->height; x++)
+        {
+            int index = 0;
+
+            for (int n = -1; n < +2; n++)
+                for (int m = -1; m < +2; m++)
+                    matrix[index++] = img_get_value(img, y + m, x + n);
+
+            qsort(matrix, window * window, sizeof(unsigned char), compare);
+
+            img->pixels[x][y] = matrix[4];
         }
     }
 }
